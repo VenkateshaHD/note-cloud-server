28e74e5b9cc4a2f82c6dee86c3c3a7edf0de0906
VenkateshaHD <venkyvenkateshahd1999@gmail.com>
2025-08-10 11:34:36 +0530

    added startup method in main

--- app/main.py
+++ app/main.py
@@ -3,9 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth, notes, upload
 from app.database import Base, engine
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+
+@app.on_event("startup")
+def startup_event():
+    # Create database tables
+    Base.metadata.create_all(bind=engine)
+    print("Creating database tables...")
+
+# Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
 app.include_router(notes.router, prefix="/api/notes", tags=["notes"])
